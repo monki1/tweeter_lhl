2ae6c44cdedda7a3c6930e3764a41512fd37ab65
monki1 <114178697+monki1@users.noreply.github.com>
2023-05-25 06:44:17 -0400

    ajax submit form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -78,3 +78,32 @@ const createTweetElement = function(tweetObj) {
 $(document).ready(function() {
     $(this).find(`main.container`).append(createTweetElement(tweetObj));
 });
+
+// ajax submit form
+//1. prevent default
+$(document).ready(function() {
+    $('form.new-tweet-form').on('submit', function(event) {
+        event.preventDefault();
+        // console.log(this);
+        // Serialize the form data
+        const serializedData = $(this).serialize();
+
+        // Submit form data using $.ajax()
+        $.ajax({
+            url: '/tweets',
+            method: 'POST',
+            data: serializedData,
+            success: function(response) {
+                // Success callback function
+                console.log('Form submitted successfully');
+                console.log('Response:', response);
+            },
+            error: function(xhr, status, error) {
+                // Error callback function
+                console.log('Error occurred while submitting the form');
+                console.log('Status:', status);
+                console.log('Error:', error);
+            }
+        });
+    });
+});
