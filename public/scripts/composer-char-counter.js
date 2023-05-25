995e604b91d3624ca465bc3ef9307f6e9e95be67
monki1 <114178697+monki1@users.noreply.github.com>
2023-05-25 03:42:32 -0400

    counter over limit turn red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
   $('#tweet-text').on('input', function() {
     const textareaValue = $(this).val();
     const counter = $(this).next('div').find('.counter');
-    counter.text(140 - textareaValue.length);
+    const remaining = 140 - textareaValue.length;
+    counter.text(remaining);
+    // if reamining is less than 0, add class 'counter-over-limit' else remove class 'counter-over-limit'
+    const overLimitClass = 'counter-over-limit';
+    if (remaining < 0) {
+        counter.addClass(overLimitClass);
+    }else{
+        counter.removeClass(overLimitClass);
+    }
     // console.log(this, counter, textareaValue);
   });
 });
